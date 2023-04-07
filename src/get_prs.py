a07e79edbcb2c1a380b4242ebb15fa50aa87cb79
Dragan Stepanović <dragan.stepanovic@gmail.com>
2023-04-07 19:20:24 +0200

    xm

--- src/get_prs.py
+++ src/get_prs.py
@@ -8,18 +8,22 @@ from github_client import GithubClient
 TIMESTAMP_FORMAT = "%d-%m-%Y_%H-%M-%S"
 
 
+def should_anonymize(do_not_anonymize):
+    return do_not_anonymize != '-plain'
+
+
 def main():
     organization = sys.argv[1]
     repository = sys.argv[2]
     number_of_prs = int(sys.argv[3])
     token = sys.argv[4]
-    do_not_anonymize = sys.argv[5]
+    to_anonymize = sys.argv[5]
 
     print(in_yellow("Getting PRs..."))
 
     prs_as_json = GithubClient.get_prs_as_json(organization, repository, number_of_prs, token)
 
-    if do_not_anonymize != '-plain':
+    if should_anonymize(to_anonymize):
         anonymized_prs, usernames_to_substitutes = Anonymizer().anonymize(prs_as_json)
         print(in_green(f'PRs saved to {save_prs(anonymized_prs)}'))
         print(in_green(f'Username substitutes saved to {save_to_file(usernames_to_substitutes, "Username_substitutes")}'))
