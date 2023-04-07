12d2b25895a8d5e8686573fb476508b412ce0588
Dragan Stepanović <dragan.stepanovic@gmail.com>
2023-04-07 19:09:57 +0200

    fix message

--- src/get_prs.py
+++ src/get_prs.py
@@ -18,8 +18,8 @@ def main():
     anonymized_prs, usernames_to_substitutes = Anonymizer() \
         .anonymize(GithubClient.get_prs_as_json(organization, repository, number_of_prs, token))
 
-    print(in_green(f'File saved to {save_to_file(anonymized_prs, "GitHub_PRs")}'))
-    print(in_green(f'File saved to {save_to_file(usernames_to_substitutes, "Username_substitutes")}'))
+    print(in_green(f'PRs saved to {save_to_file(anonymized_prs, "GitHub_PRs")}'))
+    print(in_green(f'Username substitutes saved to {save_to_file(usernames_to_substitutes, "Username_substitutes")}'))
 
 
 def save_to_file(prs_as_json, prefix):
